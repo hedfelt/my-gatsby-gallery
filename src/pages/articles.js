d40a31f590045a0e7fb70c1fe381a64a6bbc606a
Hanne  Edfelt <hanne.edfelt@gmail.com>
2021-10-28 22:11:29 +0200

    added simple article

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import BlockContent from "@sanity/block-content-to-react";
+import * as styles from "../styles/articles.module.scss";
 
-const articles = () => {
-  return <div></div>;
+const articles = ({ data }) => {
+  const blogposts = data.allSanityBlogPost.nodes;
+  return (
+    <div className={styles.articles}>
+      <div className={styles.articles__header}>Kurs Maleri 1</div>
+      <div className={styles.articles__subheader}>BLOGG</div>
+
+      {blogposts.map((blog, index) => (
+        <div key={index} className={styles.articles__post}>
+          <div>{blog.title}</div>
+          {blog.introduction.map((intro) => (
+            <BlockContent key={intro._key} blocks={intro} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default articles;
+
+export const query = graphql`
+  {
+    allSanityBlogPost {
+      nodes {
+        title
+        introduction {
+          children {
+            text
+            marks
+            _type
+            _key
+          }
+          _key
+          _type
+          list
+          style
+        }
+      }
+    }
+  }
+`;
